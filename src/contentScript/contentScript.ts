78f18521df95fa7fcf1e7eef082a9f204871357d
po-the-panda-12 <shambhavig.2019@scis.smu.edu.sg>
2025-01-31 22:35:00 +0530

    data pulled from chrome to content script - checkpoint

--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -1,43 +1,35 @@
-chrome.runtime.sendMessage('I am loading content script', (response) => {
-    console.log(response);
-    console.log('I am content script')
-
-})
-
-window.onload = (event) => {
-    console.log('page is fully loaded');
-};
-
-
-// import translate from 'translate-google-api';
-
 // chrome.runtime.sendMessage('I am loading content script', (response) => {
 //     console.log(response);
-//     console.log('I am content script');
-// });
+//     console.log('I am content script')
 
-// window.onload = async (event) => {
+// })
+
+// window.onload = (event) => {
 //     console.log('page is fully loaded');
-
-//     chrome.storage.sync.get(["language"], async (result) => {
-//         const targetLanguage = result.language || 'es'; // Default to Spanish if no language is set
-
-//         const textNodes = [];
-//         const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null);
-//         let node;
-//         while (node = walker.nextNode()) {
-//             textNodes.push(node);
-//         }
-
-//         const texts = textNodes.map(node => node.nodeValue);
-//         try {
-//             const translations = await translate(texts, { to: targetLanguage });
-//             translations.forEach((translation, index) => {
-//                 textNodes[index].nodeValue = translation;
-//             });
-//             console.log('Page translated to:', targetLanguage);
-//         } catch (error) {
-//             console.error('Translation error:', error);
-//         }
-//     });
 // };
+
+chrome.runtime.sendMessage('I am loading content script', (response) => {
+    console.log(response);
+    console.log('I am content script');
+});
+
+window.onload = (event) => {
+    console.log('Page is fully loaded');
+
+    // Retrieve "language" and "percentage" from Chrome storage
+    chrome.storage.sync.get(["language", "percentage"], (data) => {
+
+        let language = data.language || "es";  // Default to "es" (Spanish)
+        let percentage = data.percentage !== undefined ? data.percentage : 50; // Default to 50
+
+        console.log("Stored Language:", language);
+        console.log("Stored Percentage:", percentage);
+
+        // If values were not set in storage, save the default values
+        if (!data.language || data.percentage === undefined) {
+            chrome.storage.sync.set({ language, percentage }, () => {
+                console.log("Default values set in Chrome storage:", { language, percentage });
+            });
+        }
+    });
+};
